193b8fb88c2140eb3d20843876a03d7d7be6180a
btang01 <tangbri@147ddae1dd97.ant.amazon.com>
2021-09-19 11:17:55 -0400

    added hitcounter lambda function

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -3,5 +3,16 @@ import * as lambda from '@aws-cdk/aws-lambda';
 
 export interface HitCounterProps {
   /** the function for which we want to count url hits **/
+  // we will "plug in" Lambda function here to get hit-counted
   downstream: lambda.IFunction;
+}
+
+// new construct class
+export class HitCounter extends cdk.Construct {
+    constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+        super(scope, id);
+        // props argument is type HitCounterProps
+
+        // TODO
+    }
 }
